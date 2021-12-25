1bc14608d11e0ea14946e28b2b424a7377a179ae
Ynesti <vanya.malykh.2004@mail.ru>
2021-12-25 08:48:07 +0400

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main () {
+		//Set birth year
+	int birthYear = 2004;
+		//Set test year
+	int testYear = 2010;
+		//Calculate age
+	int age;
+	age = tetsYear - birthYear;
+	printf("Age of student %d in year %d\n", age,tetsYear);
+	return 0;
+}
